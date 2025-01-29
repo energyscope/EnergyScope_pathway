9313809f824671c9ebb7d16f0a825b368b600eca
Rixhon Xavier <xavier.rixhon@uclouvain.be>
2025-01-29 14:16:23 +0100

    Branching to Showcase

--- src/run_main.py
+++ src/run_main.py
@@ -43,7 +43,7 @@ CO2_neutrality_2050_val = 3406.92 # Value equivalent to CO2-neutrality in 2050
                                   # [ktCO2,eq]
                                   
 run_opti = True # True to run optimisation
-graph = False # True to plot graphs for deterministic run
+graph = True # True to plot graphs for deterministic run
 graph_comp = False # True to plot comparative graphs between two deterministic
                    # runs
 
